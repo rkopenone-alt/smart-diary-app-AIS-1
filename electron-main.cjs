71afc2939fdef932e9e95ab6102d258e1ea60bbc
rkopenone-alt <rkopenone@gmail.com>
2026-03-23 14:38:33 +0530

    Add files via upload

---
+++ electron-main.cjs
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    icon: path.join(__dirname, 'dist', 'favicon.ico'),
+  });
+
+  // In production, load the built index.html
+  // In development, load the dev server URL
+  const isDev = !app.isPackaged;
+  if (isDev) {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
